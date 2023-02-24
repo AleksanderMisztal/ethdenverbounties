77e99e5eff1c02f6f702d0bb7cf21cc135a26bbd
Aleksander Misztal <aleksandermisztal@gmail.com>
2023-02-24 14:11:47 -0700

    v0.1

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
-        <Main />
-        <NextScript />
+      <body className={'bg-gray-600 text-white'}>
+        <main className="container mx-auto w-2/3 mt-12 p-6 bg-gray-900 rounded-lg">
+          <Main />
+          <NextScript />
+        </main>
       </body>
     </Html>
-  )
+  );
 }
---
+++ pages/bounty/[id].js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import Link from 'next/link';
+
+export async function getStaticPaths() {
+  const data = require('../../public/bounties.json').challenges;
+  return {
+    paths: data.map((bounty) => {
+      return {
+        params: { id: bounty.challengeId },
+      };
+    }),
+    fallback: false, // can also be true or 'blocking'
+  };
+}
+
+export function getStaticProps(context) {
+  const id = context.params.id;
+  const bounty = require('../../public/bounties.json').challenges.find(
+    (bounty) => bounty.challengeId === id
+  );
+
+  return {
+    props: { bounty },
+  };
+}
+
+const Bounty = ({ bounty }) => {
+  const description = bounty.description.replace(/<\/?[^>]+(>|$)/g, '');
+  const acceptanceCriteria = bounty.acceptanceCriteria.replace(
+    /<\/?[^>]+(>|$)/g,
+    ''
+  );
+
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <div className="bg-green-900 rounded-lg p-2 mt-3 mb-6 w-fit">
+        <Link href="/">Back</Link>
+      </div>
+      <h1 className="text-xl font-bold">{bounty.name}</h1>
+      <h4 className="text-xl mt-6">Description</h4>
+      <p>{description}</p>
+      <h4 className="text-xl mt-6">Acceptance criteria</h4>
+      <p>{acceptanceCriteria}</p>
+      <h4 className="text-xl mt-6">Rewards</h4>
+      {bounty.rewardPool ? (
+        <p>{bounty.rewardPool}</p>
+      ) : (
+        <ul>
+          {bounty.rewards.map((reward, i) => (
+            <li key={i}>
+              {i + 1}: {reward.rewardAmountUsd} USD {reward.rewardText}
+            </li>
+          ))}
+        </ul>
+      )}
+      <button
+        className="bg-green-900 rounded-lg p-2 mt-3"
+        onClick={() => setShow(!show)}
+      >
+        {show ? 'Hide' : 'Show all info'}
+      </button>
+      {show && <p>{JSON.stringify(bounty)}</p>}
+    </>
+  );
+};
+
+export default Bounty;
